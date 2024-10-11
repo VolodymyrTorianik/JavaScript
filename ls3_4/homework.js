49deafafb25b58c919ee761b2c883b557b434fd7
VolodymyrTorianik <zvittoryanik@gmail.com>
2024-10-11 18:30:05 +0200

    Add homework Ls3_4

---
+++ ls3_4/homework.js
@@ -0,0 +1,156 @@
+//Ось приклади 8 типів данних//
+console.log("------------Ось приклади 8 типів данних------------")
+//1. Number//
+let a = 18
+console.log(a)
+
+//2. String//
+let b = 'Igor'
+console.log(b)
+
+//3. Boolean//
+let c = true
+console.log(c)
+
+//4. Null//
+let d = null
+console.log(d)
+
+//5. BigInt//
+let e = 24234234655467767546452454534534536756756754754545345435
+console.log(e)
+
+//6.Undefined//
+let f 
+console.log(f)
+//7. Symbol//
+let g = Symbol(123)
+console.log(g)
+
+//8. Object//
+
+let UserID = {
+    firstname: 'Volodymyr',
+    lastname: 'Torianik', 
+    age: 36,
+}
+console.log(UserID)
+console.log("--------10 довільних прикладів арифм опер----------------")
+
+let arifm1 = 5 + 4
+console.log(arifm1)
+
+let arifm2 = 10 - 4
+console.log(arifm2)
+
+let arifm3 = 87 + 4.56
+console.log(arifm3)
+
+let arifm4 = 25 * 1
+console.log(arifm4)
+
+let arifm5 = 125 / 2
+console.log(arifm5)
+
+let arifm6 = 3335 % 3
+console.log(arifm6)
+
+let arifm7 = 125 - 23
+console.log(arifm7)
+
+let arifm8 = 23 + 41
+console.log(arifm8)
+
+let arifm9 = 76 * 9
+console.log(arifm9)
+
+let arifm10 = 44 + 34
+console.log(arifm10)
+
+console.log("-------Приведення типів даних----------------")
+
+let datatype1 = '777'
+let numb1 = Number(datatype1)
+console.log(numb1)
+console.log(typeof numb1)
+
+let datatype2 = 123
+let numb2 = String(datatype2)
+console.log(numb2)
+console.log(typeof numb2)
+
+let datatype3 = 0
+let numb3 = Boolean(datatype3)
+console.log(numb3)
+console.log(typeof numb3)
+
+let datatype4 = 1
+let numb4 = Boolean(datatype4)
+console.log(numb4)
+console.log(typeof numb4)
+
+let datatype5 = false
+let numb5 = String(datatype5)
+console.log(numb5)
+console.log(typeof numb5)
+
+let datatype6 = null
+let numb6 = Number(datatype6)
+console.log(numb6)
+console.log(typeof numb6)
+
+console.log("-------Використанням арифметичних операторів----------------")
+
+let aX = 12
+let aY = 45
+
+let summ1 = aX + aY
+console.log(summ1)
+
+
+
+let bX = 345
+let bY = 112
+
+let summ2 = bX - bY
+console.log(summ2)
+
+
+
+
+let cX = 12
+let cY = 45
+
+let summ3 = cX * cY
+console.log(summ3)
+
+
+let dX = 12
+let dY = 45
+
+let summ4 = (12 + dX) / dY
+console.log(summ4)
+
+
+
+let eX = 12
+let eY = 45
+
+let summ5 = (eX + eY) * 3
+console.log(summ5)
+
+
+
+let fX = 20
+let fY = 3
+
+let summ6 = fX % fY
+console.log(summ6)
+
+
+
+
+
+
+
+
